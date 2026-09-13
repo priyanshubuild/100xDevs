a01fa3ddfe3a9e6fc3e067b50015080320db4bde
Priyanshu Gupta <priyanshug1408@gmail.com>
2026-09-13 23:22:11 +0530

    Learning node js basics and tried chalk module

---
+++ week-6/extra/index.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue("hello world"));
+console.log(chalk.red.bold("this is messege written in red bold"));
+console.log(chalk.green.underline("priyanshu is great"));
