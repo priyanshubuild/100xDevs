c6058892b61105210103c9c16d88e23468b5ed02
Priyanshu Gupta <priyanshug1408@gmail.com>
2026-09-15 01:07:23 +0530

    add clean text files and solved week-2 basic assignment 3

--- week-2 /assignments/basic/3.js
+++ week-2 /assignments/basic/3.js
@@ -2,11 +2,14 @@ let a={
     a:"x",
     b:"y",
     c:"z"
-}
+};
 
 let b={};
+kexs=Object.keys(a);
+console.log(kexs);
 
-for(let i=0;i<a.length;i++){
-    b[a]
+console.log(kexs[0]);
+for(let i=0;i<Object.keys(a).length;i++){
+    b[a[kexs[i]]]=kexs[i];
 }
 console.log(b);
