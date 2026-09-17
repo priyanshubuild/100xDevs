6b18be4853dc7477cc7bae594307d074970a8410
Priyanshu Gupta <priyanshug1408@gmail.com>
2026-09-17 21:55:00 +0530

    solve week-2 assignment 5: combine fruits and veggies arrays

---
+++ week-2 /assignments/basic/5.js
@@ -0,0 +1,12 @@
+let obj={ fruits: ["apple", "banana"], veggies: ["carrot", "pea"] }
+let veg=[];
+for(let i=0;i<Object.keys(obj).length;i++){
+veg[0]=obj.fruits[0];
+veg[1]=obj.fruits[1];
+veg[2]=obj.veggies[0];
+veg[3]=obj.veggies[1];
+
+
+
+}
+console.log(veg);
