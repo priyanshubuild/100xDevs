6e0eeb2935c7bb4a70b1ac77b5bd2327130db163
Priyanshu Gupta <priyanshug1408@gmail.com>
2026-09-17 23:40:00 +0530

    create basic express http server

---
+++ week-6/extra/http/index.js
@@ -0,0 +1,15 @@
+//creating an http server
+//express
+//node default liberary =>no
+
+const express = require("express");
+
+const app = express();
+
+app.get("/",function(req,res){
+
+res.send("hi there"); 
+})
+
+app.listen(3000);
+ 
