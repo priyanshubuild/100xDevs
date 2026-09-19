6bedcbe8331747c8c71fa7127216ee7e84a77ad3
Priyanshu Gupta <priyanshug1408@gmail.com>
2026-09-19 19:37:16 +0530

    solve week-2 assignment 16: count even and odd numbers in array

---
+++ week-2 /assignments/basic/16.js
@@ -0,0 +1,16 @@
+let arr=[1,2,3,4,5,6];
+
+let even=0;
+let odd=0;
+for (let i=0;i<arr.length;i++){
+    if(arr[i]%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+}
+let newobj={};
+newobj["even"]=even;
+newobj["odd"]=odd;
+console.log(newobj);
